06e0e80de035b52224224365cd3dc464eb0d70bb
Tushar Gaurav <tushar.gaurav416@gmail.com>
2026-05-11 17:17:59 +0530

    chore: assets update

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -55,9 +55,18 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-              <div className="flex items-center pt-2 pl-1">
-                <img src="/logo.png" alt="logo" className="object-contain max-w-30" />
-              </div>
+            <div className="relative flex h-10 items-center pt-2 pl-1 group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:pl-0">
+              <img
+                src="/logo.png"
+                alt="Inboundr"
+                className="max-w-30 object-contain opacity-100 transition-[opacity,transform] duration-200 ease-linear group-data-[collapsible=icon]:scale-95 group-data-[collapsible=icon]:opacity-0"
+              />
+              <img
+                src="/mark.png"
+                alt="Inboundr"
+                className="absolute size-7 scale-90 object-contain opacity-0 transition-[opacity,transform] duration-200 ease-linear group-data-[collapsible=icon]:scale-100 group-data-[collapsible=icon]:opacity-100"
+              />
+            </div>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
